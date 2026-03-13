0b070c8aea4cc49df33df58385c505a19f4dbe96
Blackcockatoo <158782528+Blackcockatoo@users.noreply.github.com>
2026-03-13 11:29:11 +1100

    Improve campaign site accessibility and add /app route

--- 06-Campaign-Site/src/App.tsx
+++ 06-Campaign-Site/src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Switch } from "wouter";
 import IndexPage from "./pages/IndexPage";
 import ParentsPage from "./pages/ParentsPage";
+import AppPage from "./pages/AppPage";
 import SchoolsPage from "./pages/SchoolsPage";
 import InvestorsPage from "./pages/InvestorsPage";
 import ElevatorPage from "./pages/ElevatorPage";
@@ -9,6 +10,7 @@ export default function App() {
   return (
     <Switch>
       <Route path="/" component={IndexPage} />
+      <Route path="/app" component={AppPage} />
       <Route path="/parents" component={ParentsPage} />
       <Route path="/schools" component={SchoolsPage} />
       <Route path="/investors" component={InvestorsPage} />
